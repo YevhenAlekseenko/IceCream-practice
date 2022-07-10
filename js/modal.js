8f41b3184b8fb99c98904db35f4eba8188a90019
YevhenAlekseenko <alekseenko_@ukr.net>
2022-07-10 15:52:46 +0300

    add about-modal

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,19 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[about-modal-open]"),
+    closeModalBtn: document.querySelector("[about-modal-close]"),
+    modal: document.querySelector("[about-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
